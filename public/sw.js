0da5b9423967a502f7f1199d37fe9b74feeef0fc
SISUMEPE <sisumepe@local>
2026-09-16 13:45:58 -0300

    fix: PDF termos sem sobreposicao - coordenadas exatas do original

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'te-v21';
+const CACHE = 'te-v22';
 const CORE = ['/', '/manifest.json', '/icons/icon.svg', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
