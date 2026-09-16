fabc35ac362978fb008fee5bab9ec0f38d69d436
SISUMEPE <sisumepe@local>
2026-09-16 10:28:11 -0300

    feat: recorte livre de imagem no anexo de ticket
    - Modal digitalizar agora com area de recorte arrastavel/redimensionavel
    - Toggle Recortar ON/OFF, Reset, hint e overlay com alças
    - attachScan respeita cropRect (recorte livre) antes de anexar
    - Bump sw para te-v5

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'te-v4';
+const CACHE = 'te-v5';
 const CORE = ['/', '/manifest.json', '/icons/icon.svg', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
