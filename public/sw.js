926faadaaf6557345adb2ec48df3574d0a082f9f
SISUMEPE <sisumepe@local>
2026-09-22 21:14:26 -0300

    fix(auth): elimina flash da tela principal antes do login (loader + appShell hidden até check)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'te-v33';
+const CACHE = 'te-v34';
 const CORE = ['/', '/manifest.json', '/icons/icon.svg', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
